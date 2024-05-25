7a080f83c824fcb2500cd0313ab6e6b331671ebb
karelindeveloper <karelin.developer@gmail.com>
2024-05-25 11:10:14 +0500

    Update 2024-05-25T06:09:27.192Z

--- src/js/_components.js
+++
@@ -1,6 +0,0 @@
-import "./components/banners";
-import "./components/ornaments";
-import "./components/parallax";
-import "./components/stories";
-import "./components/swiper";
-import "./components/timeline";
